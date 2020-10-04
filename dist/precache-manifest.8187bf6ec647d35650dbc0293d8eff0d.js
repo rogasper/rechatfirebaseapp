cf6f68bf9f4c75170c8c20840477f196d06af286
rogasper <royangagas@gmail.com>
2020-10-04 09:22:28 +0700

    Finish

---
+++ dist/precache-manifest.8187bf6ec647d35650dbc0293d8eff0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01901221950353970b09c08f53d71ae1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "53009ee00326c745e7f0",
+    "url": "/static/css/main.511a946a.chunk.css"
+  },
+  {
+    "revision": "fad1796ce4a164da6318",
+    "url": "/static/js/2.6a7c387f.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "/static/js/2.6a7c387f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53009ee00326c745e7f0",
+    "url": "/static/js/main.c467876c.chunk.js"
+  },
+  {
+    "revision": "872fdadcb9dfb1edf0f6",
+    "url": "/static/js/runtime-main.8a7878ae.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,117 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+import './index.css'
+
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
+firebase.initializeApp({
+  // your config
+})
+
+const auth = firebase.auth()
+const firestore = firebase.firestore()
 
 function App() {
+
+  const [user] = useAuthState(auth);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Rechat App</h1>
+        <SignOut/>
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+        {user ? '' : 
+    <p className="credit">Aplikasi Chat by <a href="https://instagram.com/rogasper" target="_blank">Rogasper</a></p>}
+      </section>
+
+
     </div>
-  );
+    )
+}
+
+function SignIn(){
+
+  const signInWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider()
+    auth.signInWithPopup(provider)
+  }
+
+  return (
+    <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+  )
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button onClick={() => auth.signOut()}>Sign Out</button>
+  )
+}
+
+function ChatRoom() {
+  const messageRef = firestore.collection('messages')
+  const query = messageRef.orderBy('createdAt').limitToLast(25)
+
+  const [messages] = useCollectionData(query, {idField: 'id'});
+
+  const [formValue, setFormValue] = useState('')
+
+  const sendMessage = async (e) => {
+    e.preventDefault()
+
+    const { uid, photoURL } = auth.currentUser
+
+    await messageRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('')
+  }
+
+  const dummy = useRef()
+
+  useEffect(() => {
+    dummy.current.scrollIntoView({behavior: 'smooth'})
+  }, [messages])
+
+  return(
+    <>
+      <main>
+        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+        <span ref={dummy}></span>
+      </main>
+
+      <form onSubmit={sendMessage}>
+        <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Katakan Halo"/>
+        <button type="submit" disabled={!formValue}>Kirim</button>
+      </form>
+    </>
+  )
+}
+
+function ChatMessage({ message }) {
+  const { text, uid, photoURL } = message;
+
+  const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
+
+  return(
+    <>
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'}/>
+        <p>{text}</p>
+      </div>
+    </>
+  )
 }
 
 export default App;
